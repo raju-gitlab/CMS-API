838f32c958685e9d63757f5a51878db581296368
Raju.b@Basak <63355575+raju-gitlab@users.noreply.github.com>
2022-08-05 18:41:09 +0530

    Add project files.

---
+++ CMS.MODEL/Master/MeetingModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.MODEL.Master
+{
+    public class MeetingModel : BaseModel
+    {
+        public string MeetingName { get; set; }
+        public string MeetingDate { get; set; }
+        public string MeetingTime { get; set; }
+        public string MeetingLink { get; set; }
+        public string Stream { get; set; }
+        public string Semester { get; set; }
+        public string MeetingId { get; set; }
+        public bool IsAttended { get; set; }
+    }
+}
